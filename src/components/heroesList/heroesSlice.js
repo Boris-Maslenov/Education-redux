0ab21b12ce28c9b7cc90bbdf1fe70da909c0d053
Boris-Maslenov <ya.maslenov@yandex.ru>
2022-09-15 09:58:17 +0300

    add  create async thunk

--- src/components/heroesList/heroesSlice.js
+++ src/components/heroesList/heroesSlice.js
@@ -1,9 +1,20 @@
-import {createSlice, nanoid} from '@reduxjs/toolkit';
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import {useHttp} from '../../hooks/http.hook';
 
 const initialState = {
     heroes: [],
     heroesLoadingStatus: 'idle',
 }
+
+export const fetchHeroes = createAsyncThunk( // тут вернутся не 1 action Creator а 3!! Которые описывают состояние нашего промиса
+    'heroes/fetchHeroes', 
+    async (type, payloadCreator) => {
+        //Обязательно должна вернуть Promise/ Иначе вручную создавать ошибку
+        const {request} = useHttp(); // если функция обернуты в callback то работать не будет!
+        // В нашем случае можно без async/await
+        return await request('http://localhost:3001/heroes') 
+    }
+);
 
 // createSlice вернет 3 сущности:
 // 1. Имя среза
@@ -13,14 +24,15 @@ const heroesSlice = createSlice({
     name: 'heroes',
     initialState,
     reducers: {
-        heroesFetching: state => {
-        state.heroesLoadingStatus = 'loading';
-        },
-        heroesFetched: (state, action) => {
-            state.heroesLoadingStatus = 'idle';
-            state.heroes = action.payload;
-        },
-        heroesFetchingError: state => {state.heroesLoadingStatus = 'error'},
+
+        // heroesFetching: state => {
+        // state.heroesLoadingStatus = 'loading';
+        // },
+        // heroesFetched: (state, action) => {
+        //     state.heroesLoadingStatus = 'idle';
+        //     state.heroes = action.payload;
+        // },
+        // heroesFetchingError: state => {state.heroesLoadingStatus = 'error'},
         heroCreated: (state, action) => {
             state.heroes.push(action.payload);
         },
@@ -39,6 +51,16 @@ const heroesSlice = createSlice({
         //     }
         // }
 
+    },
+    extraReducers: (builder) => {
+        builder 
+            .addCase(fetchHeroes.pending,  state => {state.heroesLoadingStatus = 'loading'})
+            .addCase(fetchHeroes.fulfilled, (state, action) => {
+                        state.heroesLoadingStatus = 'idle';
+                        state.heroes = action.payload; // сюда придут данные полученные с запроса автоматически
+            })
+            .addCase(fetchHeroes.rejected, state => {state.heroesLoadingStatus = 'error'})
+            .addDefaultCase(()=>{})
     }
 
 });
